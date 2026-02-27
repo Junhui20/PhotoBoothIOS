b9cdcfef91928404abb1efa673e27382f582fc5d
Junhui20 <huijun00100101@gmail.com>
2026-02-27 18:03:16 +0800

    Initial Commit

---
+++ PhotoBoothIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoBoothIOS
+//
+//  Created by IM-MacMini-1 on 27/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoBoothIOS/PhotoBoothIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoBoothIOSApp.swift
+//  PhotoBoothIOS
+//
+//  Created by IM-MacMini-1 on 27/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoBoothIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
